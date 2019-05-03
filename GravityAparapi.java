63e081a241aed82b38ede092b7f92f38dab4276e
Zak Dowsett <z.dowsett@outlook.com>
2019-05-03 17:35:21 +0100

    Update GravityAparapi.java

--- GravityAparapi.java
+++ GravityAparapi.java
@@ -1,73 +1,79 @@
-
 /*
- * "Physics" part of code adapted from Dan Schroeder's applet at:
- *
- *     http://physics.weber.edu/schroeder/software/mdapplet.html
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-import com.amd.aparapi.Kernel;
-import com.amd.aparapi.Range;
-import com.amd.aparapi.device.Device;
+package gravityaparapi;
 
-import java.awt.* ;
-import javax.swing.* ;
+import com.aparapi.Kernel;
+import com.aparapi.Range;
+import com.aparapi.device.Device;
 
+import java.awt.*;
+import java.awt.Color;
+import java.util.concurrent.CyclicBarrier;
+import javax.swing.*;
+
+/**
+ *
+ * @author up788458
+ */
 public class GravityAparapi {
-   
+
+    final static int THREADS = 8;
+    static CyclicBarrier Barrier = new CyclicBarrier(THREADS);
     // Size of simulation
 
-    final static int N = 2000 ;  // Number of "stars"
-    final static double BOX_WIDTH = 100.0 ;
-
+    final static int N = 6000;  // Number of "stars"
+    final static double BOX_WIDTH = 100.0;
 
     // Initial state
+    final static double RADIUS = 20.0;  // of randomly populated sphere
 
-    final static double RADIUS = 20.0 ;  // of randomly populated sphere
-
-    final static double ANGULAR_VELOCITY = 0.4 ;
-           // controls total angular momentum
-
+    final static double ANGULAR_VELOCITY = 0.4;
+    // controls total angular momentum
 
     // Simulation
-
-    final static double DT = 0.002 ;  // Time step
-
+    final static double DT = 0.002;  // Time step
 
     // Display
+    final static int WINDOW_SIZE = 800;
+    final static int DELAY = 0;
+    final static int OUTPUT_FREQ = 25;
 
-    final static int WINDOW_SIZE = 1000 ;
-    final static int DELAY = 0 ;
-    final static int OUTPUT_FREQ = 5 ;
-
-
+    // non final static variables are not supported, will have to init and make during execution
     // Star positions
-    static double [] x = new double [N] ;
-    static double [] y = new double [N] ;
-    static double [] z = new double [N] ;
-
-    // Star velocities
-    static double [] vx = new double [N] ;
-    static double [] vy = new double [N] ;
-    static double [] vz = new double [N] ;
-
-    // Star accelerations
-    static double [] ax = new double [N] ;
-    static double [] ay = new double [N] ;
-    static double [] az = new double [N] ;
-
-
-    public static void main(String args []) throws Exception {
-
-        // start gpu shite
-        Device gpu = Device.firstGPU();
-        System.out.println(gpu.toString());
+//    static double[] x = new double[N];
+//    static double[] y = new double[N];
+//    static double[] z = new double[N];
+//
+//    // Star velocities
+//    static double[] vx = new double[N];
+//    static double[] vy = new double[N];
+//    static double[] vz = new double[N];
+//
+//    // Star accelerations
+//    static double[] ax = new double[N];
+//    static double[] ay = new double[N];
+//    static double[] az = new double[N];
+    public static void main(String args[]) throws Exception {
         
-        Display display = new Display() ;
+        // Star positions
+        double[] x = new double[N];
+        double[] y = new double[N];
+        double[] z = new double[N];
 
-        // Define initial state of stars
+        // Star velocities
+        double[] vx = new double[N];
+        double[] vy = new double[N];
+        double[] vz = new double[N];
 
-        // Randomly choose plane for net angular velocity
-
-        //nx, ny, nz represents angular vel (or rotation speed)
+        // Star accelerations
+        double[] ax = new double[N];
+        double[] ay = new double[N];
+        double[] az = new double[N];
+        
+        // generate stars in the galaxy initial pos and angular velocity
         double nx = 2 * Math.random() - 1;
         double ny = 2 * Math.random() - 1;
         double nz = 2 * Math.random() - 1;
@@ -76,48 +82,91 @@ public class GravityAparapi {
         ny *= norm;
         nz *= norm;
 
-        
-
-        // ... or just rotate in x, y plane
-        //double nx = 0, ny = 0, nz = 1.0 ;
-
-        // ... or just rotate in x, z plane
-        //double nx = 0, ny = 1.0, nz = 0 ;
-
-        for(int i = 0 ; i < N ; i++) {
+        for (int i = 0; i < N; i++) {
 
             // Place star randomly in sphere of specified radius
-            double rx, ry, rz, r ;
+            double rx, ry, rz, r;
             do {
-                rx = (2 * Math.random() - 1) * RADIUS ;
-                ry = (2 * Math.random() - 1) * RADIUS ;
-                rz = (2 * Math.random() - 1) * RADIUS ;
-                r = Math.sqrt(rx * rx + ry * ry + rz * rz) ;
-            } while(r > RADIUS) ;
+                rx = (2 * Math.random() - 1) * RADIUS;
+                ry = (2 * Math.random() - 1) * RADIUS;
+                rz = (2 * Math.random() - 1) * RADIUS;
+                r = Math.sqrt(rx * rx + ry * ry + rz * rz);
+            } while (r > RADIUS);
 
-            x [i] = 0.5 * BOX_WIDTH + rx ;
-            y [i] = 0.5 * BOX_WIDTH + ry ;
-            z [i] = 0.5 * BOX_WIDTH + rz ;
+            x[i] = 0.5 * BOX_WIDTH + rx;
+            y[i] = 0.5 * BOX_WIDTH + ry;
+            z[i] = 0.5 * BOX_WIDTH + rz;
 
-            vx [i] = ANGULAR_VELOCITY * (ny * rz - nz * ry) ; 
-            vy [i] = ANGULAR_VELOCITY * (nz * rx - nx * rz) ; 
-            vz [i] = ANGULAR_VELOCITY * (nx * ry - ny * rx) ; 
+            vx[i] = ANGULAR_VELOCITY * (ny * rz - nz * ry);
+            vy[i] = ANGULAR_VELOCITY * (nz * rx - nx * rz);
+            vz[i] = ANGULAR_VELOCITY * (nx * ry - ny * rx);
         }
-         
+        
+        Display display = new Display();
+        
+        Device gpu = Device.best();
+        System.out.println(gpu.toString());
 
-        int iter = 0 ;
+        // create kernel here
+        Kernel kernel = new Kernel() {
+            @Override
+            public void run() {
+                int globalid = getGlobalId();
+
+                double dx, dy, dz;  // separations in x and y directions
+                double dx2, dy2, dz2, rSquared, r, rCubedInv, fx, fy, fz;
+
+                // Interaction forces (gravity)
+                for (int j = 0; j < N; j++) {  // loop over all distinct pairs
+
+                    // does not support break >:(
+                    //if (globalid == j) 
+                    //    break;
+                    if (globalid != j) {
+                        // Vector version of inverse square law
+                        dx = x[globalid] - x[j];
+                        dy = y[globalid] - y[j];
+                        dz = z[globalid] - z[j];
+                        dx2 = dx * dx;
+                        dy2 = dy * dy;
+                        dz2 = dz * dz;
+                        rSquared = dx2 + dy2 + dz2;
+                        r = Math.sqrt(rSquared);
+                        rCubedInv = 1.0 / (rSquared * r);
+                        fx = -rCubedInv * dx;
+                        fy = -rCubedInv * dy;
+                        fz = -rCubedInv * dz;
+
+                        ax[globalid] = fx;  // add this force on to i's acceleration (mass = 1)
+                        ay[globalid] = fy;
+                        az[globalid] = fz;
+                        // ax[j] -= fx;  // Newton's 3rd law
+                        //  ay[j] -= fy;
+                        //  az[j] -= fz;
+                    }
+                }
+
+            }
+        ;
+
+        };                
+        
+        //Range range = Range.create(N);
+        Range range = gpu.createRange(N);
+
         long startTime = System.currentTimeMillis();
-        while(iter < 2000) {
-
-            if(iter % OUTPUT_FREQ == 0) {
-                System.out.println("iter = " + iter + ", time = " + iter * DT) ;
-                display.repaint() ;
-                Thread.sleep(DELAY) ;
+               // Thread.sleep(10000);
+        
+        int iter = 0;
+        while (iter < 2000) {
+            if (iter % OUTPUT_FREQ == 0) {
+                System.out.println("iter = " + iter + ", time = " + iter * DT);
+                display.UpdatePos(x, y);
+                display.repaint();
             }
 
             // Verlet integration:
             // http://en.wikipedia.org/wiki/Verlet_integration#Velocity_Verlet
-
             double dtOver2 = 0.5 * DT;
             double dtSquaredOver2 = 0.5 * DT * DT;
             for (int i = 0; i < N; i++) {
@@ -131,79 +180,42 @@ public class GravityAparapi {
                 vz[i] += (az[i] * dtOver2);
             }
 
-            computeAccelerations();
+            // need to look at the axis in this as well
+            //computeAccelerations();
+            // a kernel for each i iteration
+            // gid represents what i is
+            // replaces compute acceleratinos mthod
+            kernel.execute(range);
 
+            // update velocities with new acceleration
             for (int i = 0; i < N; i++) {
-                // finish updating velocity with new acceleration
                 vx[i] += (ax[i] * dtOver2);
                 vy[i] += (ay[i] * dtOver2);
                 vz[i] += (az[i] * dtOver2);
             }
 
-            iter++ ;
+            iter++;
         }
-      
+
         long elapsedTime = System.currentTimeMillis() - startTime;
-        System.out.println("Aparapi Elapsed Time = " + elapsedTime + "ms");
+
+        System.out.println("GPU Elapsed Time = " + elapsedTime + "ms");
+        System.exit(0); // lazy me forgets to close the jframe
     }
-
-    // Compute accelerations of all stars from current positions:
-    static void computeAccelerations() {
-
-        double dx, dy, dz;  // separations in x and y directions
-        double dx2, dy2, dz2, rSquared, r, rCubedInv, fx, fy, fz;
-
-        for (int i = 0; i < N; i++) {
-            ax[i] = 0.0;
-            ay[i] = 0.0;
-            az[i] = 0.0;
-        }
-
-        // Interaction forces (gravity)
-        // This is where the program spends most of its time.
-
-        // (NOTE: use of Newton's 3rd law below to essentially half number
-        // of calculations needs some care in a parallel version.
-        // A naive decomposition on the i loop can lead to a race condition
-        // because you are assigning to ax[j], etc.
-        // You can remove these assignments and extend the j loop to a fixed
-        // upper bound of N, or, for extra credit, find a cleverer solution!)
-
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < i; j++) {  // loop over all distinct pairs
-
-                // Vector version of inverse square law
-                dx = x[i] - x[j];
-                dy = y[i] - y[j];
-                dz = z[i] - z[j];
-                dx2 = dx * dx;
-                dy2 = dy * dy;
-                dz2 = dz * dz;
-                rSquared = dx2 + dy2 + dz2 ;
-                r = Math.sqrt(rSquared) ;
-                rCubedInv = 1.0 / (rSquared * r) ;
-                fx = - rCubedInv * dx;
-                fy = - rCubedInv * dy;
-                fz = - rCubedInv * dz;
-
-                ax[i] += fx;  // add this force on to i's acceleration (mass = 1)
-                ay[i] += fy;
-                az[i] += fz;
-                ax[j] -= fx;  // Newton's 3rd law
-                ay[j] -= fy;
-                az[j] -= fz;
-            }
-        }
-    }
-
 
     static class Display extends JPanel {
 
-        static final double SCALE = WINDOW_SIZE / BOX_WIDTH ;
-
+        static final double SCALE = WINDOW_SIZE / BOX_WIDTH;
+        double[] x = new double[N];
+        double[] y = new double[N];
+        
         Display() {
+            for (int i = 0; i < N; i++){             
+                x[i] = 0;
+                y[i] = 0;
+            }
 
-            setPreferredSize(new Dimension(WINDOW_SIZE, WINDOW_SIZE)) ;
+            setPreferredSize(new Dimension(WINDOW_SIZE, WINDOW_SIZE));
 
             JFrame frame = new JFrame("MD");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -211,19 +223,64 @@ public class GravityAparapi {
             frame.pack();
             frame.setVisible(true);
         }
+        
+        public void UpdatePos(double[] x, double[] y){
+            this.x = x;
+            this.y = y;
+        }
 
+        @Override
         public void paintComponent(Graphics g) {
-            g.setColor(Color.BLACK) ;
-            g.fillRect(0, 0, WINDOW_SIZE, WINDOW_SIZE) ;
-            g.setColor(Color.WHITE) ;
-            for(int i = 0 ; i < N ; i++) {
-                int gx = (int) (SCALE * x [i]) ;
-                int gy = (int) (SCALE * y [i]) ;
-                if(0 <= gx && gx < WINDOW_SIZE && 0 < gy && gy < WINDOW_SIZE) { 
-                    g.fillRect(gx, gy, 1, 1) ;
-                }
-            } 
+            g.setColor(Color.BLACK);
+            g.fillRect(0, 0, WINDOW_SIZE, WINDOW_SIZE);
+            g.setColor(Color.WHITE);
+            for (int i = 0; i < N; i++) {
+
+                // choose colour based on thread
+                // comment out to keep white
+                //g.setColor(GetThreadColour(i));
+                
+                // code cannot be fetched from non static field, will nedd to update      
+              
+                    try {
+                        int gx = (int) (SCALE * x[i]);
+                        int gy = (int) (SCALE * y[i]);
+
+                        if (0 <= gx && gx < WINDOW_SIZE && 0 < gy && gy < WINDOW_SIZE) 
+                            g.fillRect(gx, gy, 1, 1);
+                    }
+                    catch (NullPointerException e) {
+                        System.out.println(i + " is null! WTF");
+                        continue;
+                    }
+                    
+                
+                
+            }
+        }
+
+        private Color GetThreadColour(int i) {
+            double threadNo = i / (N / (double) THREADS);
+            if (threadNo <= 1) {
+                return Color.RED;
+            } else if (threadNo <= 2) {
+                return Color.WHITE;
+            } else if (threadNo <= 3) {
+                return Color.GREEN;
+            } else if (threadNo <= 4) {
+                return Color.BLUE;
+            } else if (threadNo <= 5) {
+                return Color.ORANGE;
+            } else if (threadNo <= 6) {
+                return Color.PINK;
+            } else if (threadNo <= 7) {
+                return Color.MAGENTA;
+            } else if (threadNo <= 8) {
+                return Color.CYAN;
+            } else {
+                throw new UnsupportedOperationException();
+            }
         }
     }
+
 }
-    
